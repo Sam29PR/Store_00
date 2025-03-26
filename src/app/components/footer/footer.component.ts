74e345b9fde68025ae2fd0222c8331f1ca072b69
samit <samithpa04@gmail.com>
2025-03-26 01:59:47 -0500

    Inicializacion del proyecto tienda virtual

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+  imports: [],
+
+})
+export class FooterComponent {
+
+}
